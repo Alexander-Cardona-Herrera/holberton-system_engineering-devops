afb3188c53234ee8d47a6c61dd9d900905d6285a
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-17 16:43:59 -0500

    adding task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # changing the configuration 
 
-file_line {'ssh_config':
+file_line {'ssh_config password':
   path    => '/etc/ssh/ssh_config',
-  line    => "    PasswordAuthentication no",
-  replace => true
+  line    => '    PasswordAuthentication no',
+  replace => 'true'
 }
 
-file_line {'ssh_config':
+file_line {'ssh_config identity':
   path    => '/etc/ssh/ssh_config',
-  line    => "    IdentityFile ~/.ssh/holberton",
-  replace => true
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => 'true'
 }
