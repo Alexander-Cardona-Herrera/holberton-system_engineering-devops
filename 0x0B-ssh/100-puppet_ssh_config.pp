060c1fabb3bb41e5ef77c354cd05704c21cdd05b
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-17 16:29:35 -0500

    puppet-lint

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # changing the configuration 
 
 file_line {'ssh_config':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
+  path	=> '/etc/ssh/ssh_config',
+  line	=> 'PasswordAuthentication no',
 }
 
 file_line {'ssh_config':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/holberton',
+  path	=> '/etc/ssh/ssh_config',
+  line	=> 'IdentityFile ~/.ssh/holberton',
 }
