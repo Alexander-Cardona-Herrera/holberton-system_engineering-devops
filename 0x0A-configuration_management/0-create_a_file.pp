c0a4fcb368fe427f1a8d0cd30dc98d070de6c3f7
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-17 11:19:01 -0500

    adding task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# create a temporal file
+
 file {'/tmp/holberton':
-    content => 'I love Puppet',
-    group   => www-data,
-    mode    => '0744',
-    owner   => www-data,
+  content => 'I love Puppet',
+  group   => www-data,
+  mode    => '0744',
+  owner   => www-data,
 }
