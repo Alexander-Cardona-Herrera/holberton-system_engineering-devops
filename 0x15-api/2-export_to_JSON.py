5c2727ce85e7e21417c9f814017cc132c895772d
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-13 16:29:18 -0500

    adding task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Module to export to csv.
+"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}"
+        .format(sys.argv[1])).json()
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}"
+        .format(sys.argv[1])).json()
+
+    all_tasks = []
+    for tasks in response:
+        all_tasks.append({"task": tasks.get('title'), "completed": tasks.get(
+            'completed'), "username": name.get('username')})
+    user_dict = {"{}".format(sys.argv[1]): all_tasks}
+    print(user_dict)
+    with open('{}.json'.format(sys.argv[1]), 'w') as f:
+        json.dumps(user_dict, f)
