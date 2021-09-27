49990d66783c63f1ca3a66652ddfad6642ab322a
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-27 18:44:31 -0500

    adding task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ import json
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """ function that recibe the tittle of the subreddit
         an return the total number of hot articles. """
 
@@ -16,7 +16,7 @@ def recurse(subreddit, hot_list=[]):
             AppleWebKit/537.36 (KHTML, like Gecko)\
                 Chrome/94.0.4606.61 Safari/537.36"}
     response = requests.get(
-        "https://www.reddit.com/r/{}/hot/.json".format(
+        "https://www.reddit.com/r/{}/hot.json?limit=100".format(
             subreddit), headers=headers)
     res = response.json()
 
@@ -25,4 +25,7 @@ def recurse(subreddit, hot_list=[]):
     else:
         for post in res.get("data").get("children"):
             hot_list.append((post.get("data").get("title")))
+        after = res.get("data").get("after")
+        if after is not None:
+            recurse(subreddit, hot_list, after)
         return hot_list
