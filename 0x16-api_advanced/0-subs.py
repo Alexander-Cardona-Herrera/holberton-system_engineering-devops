7d1695fec0698d62e7a2d4119d5a77f7c7ab9739
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-27 15:47:03 -0500

    adding pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ function that recibe the tittle of the subreddit 
+    """ function that recibe the tittle of the subreddit
         an return the total number of subscribers. """
 
     headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"}
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+                Chrome/94.0.4606.61 Safari/537.36"}
     response = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit), headers=headers)
+        "https://www.reddit.com/r/{}/about.json".format(
+            subreddit), headers=headers)
     res = response.json()
 
     if response.status_code is not 200:
