f4f9d6b72fb2180baf94d0afc5912d4e89f36ceb
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-29 17:14:34 -0500

    puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # automation to find a replace of a bad file extension
 exec {'fix phpp':
-    command  => 'sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php'
+    command  => 'sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
     provider => shell
 }
