0d50f82a2bce653696c9962537eedb056d220ea5
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-29 17:11:25 -0500

    adding task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# automation of the find a replace of a bad file extension
+# automation to find a replace of a bad file extension
 exec {'fix phpp':
-    command => 'sudo sed -i "s/phpp/php" /var/www/html/wp-settings.php'
-    provider => shell,
+    command => 'sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php'
+    provider => shell
 }
